b28365adc0fba49b24966ae657cf69c70d1b686e
jtmeyer98 <jtmeyer@uc.cl>
2024-05-06 13:09:14 -0400

    chore: agregar proxy

--- src/components/PurchaseRequestTable.vue
+++ src/components/PurchaseRequestTable.vue
@@ -57,8 +57,7 @@ export default {
       this.expanded = this.expanded === id ? null : id;
     },
     fetchPurchaseRequests() {
-      // axios.get('http://localhost:8000/api/purchase_requests/')
-      axios.get('https://api-manager-x6lx.onrender.com/api/purchase_requests/') // al sacar la s de http, se puede ver el error en la consola del navegador
+      axios.get('http://localhost:8080/api/purchase_requests/')
         .then(response => {
           this.purchaseRequests = response.data;
         })
