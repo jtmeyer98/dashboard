1a1009de679b0eb6b583bbb37ad3adc58c2cdd97
jtmeyer98 <jtmeyer@uc.cl>
2024-05-06 12:18:42 -0400

    fix: sacar s de https

--- src/components/PurchaseRequestTable.vue
+++ src/components/PurchaseRequestTable.vue
@@ -58,7 +58,7 @@ export default {
     },
     fetchPurchaseRequests() {
       // axios.get('http://localhost:8000/api/purchase_requests/')
-      axios.get('https://api-manager-x6lx.onrender.com/api/purchase_requests/') // al sacar la s de http, se puede ver el error en la consola del navegador
+      axios.get('http://api-manager-x6lx.onrender.com/api/purchase_requests/') // al sacar la s de http, se puede ver el error en la consola del navegador
         .then(response => {
           this.purchaseRequests = response.data;
         })
