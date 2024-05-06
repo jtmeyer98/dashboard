df2e9eccf256613b5678966fee2c75ef133fb9fd
jtmeyer98 <jtmeyer@uc.cl>
2024-05-06 17:38:46 -0400

    feat: agregar paginación bien

--- src/components/PurchaseRequestTable.vue
+++ src/components/PurchaseRequestTable.vue
@@ -36,17 +36,13 @@
         </tr>
       </tbody>
     </table>
-    <!-- Pagination Controls -->
     <nav aria-label="Page navigation example">
       <ul class="pagination">
-        <li class="page-item" :class="{ disabled: currentPage === 1 }">
-          <a class="page-link" href="#" @click="changePage(currentPage - 1)">Previous</a>
+        <li class="page-item" :class="{ disabled: currentPage <= 1 }">
+          <button class="page-link" @click="currentPage--" :disabled="currentPage <= 1">Anterior</button>
         </li>
-        <li class="page-item" :class="{ active: page === currentPage }" v-for="page in totalPages" :key="page">
-          <a class="page-link" href="#" @click="changePage(page)">{{ page }}</a>
-        </li>
-        <li class="page-item" :class="{ disabled: currentPage === totalPages }">
-          <a class="page-link" href="#" @click="changePage(currentPage + 1)">Next</a>
+        <li class="page-item" :class="{ disabled: currentPage >= pageCount }">
+          <button class="page-link" @click="currentPage++" :disabled="currentPage >= pageCount">Siguiente</button>
         </li>
       </ul>
     </nav>
@@ -61,46 +57,43 @@ export default {
     return {
       expanded: null,
       purchaseRequests: [],
+      allRequests: [],
       currentPage: 1,
-      perPage: 10
+      perPage: 10,
+      pageCount: 0,
     };
-  },
-  computed: {
-    totalPages() {
-      return Math.ceil(this.purchaseRequests.length / this.perPage);
-    },
-    paginatedRequests() {
-      const start = (this.currentPage - 1) * this.perPage;
-      const end = start + this.perPage;
-      return this.purchaseRequests.slice(start, end);
-    }
   },
   mounted() {
     this.fetchPurchaseRequests();
   },
   methods: {
-    toggleDetails(id) {
-      this.expanded = this.expanded === id ? null : id;
-    },
     fetchPurchaseRequests() {
-      axios.get('https://api-manager-x6lx.onrender.com/api/purchase_requests/') // configuración para producción
+      axios.get('https://api-manager-x6lx.onrender.com/api/purchase_requests/')
         .then(response => {
-          console.log('Purchase requests:', response.data);
-          this.purchaseRequests = response.data;
+          this.allRequests = response.data;
+          this.pageCount = Math.ceil(this.allRequests.length / this.perPage);
+          this.updatePage();
         })
         .catch(error => {
           console.error('There was an error fetching the purchase requests:', error);
         });
     },
-    changePage(page) {
-      if (page > 0 && page <= this.totalPages) {
-        this.currentPage = page;
-      }
+    updatePage() {
+      const start = (this.currentPage - 1) * this.perPage;
+      const end = start + this.perPage;
+      this.purchaseRequests = this.allRequests.slice(start, end);
+    },
+    toggleDetails(id) {
+      this.expanded = this.expanded === id ? null : id;
     }
+  },
+  watch: {
+    currentPage() {
+      this.updatePage();
+    },
   }
 }
 </script>
-
 
 <style>
 .details {
